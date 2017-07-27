7bd71112baf0ead0d07dd46fd33724fa5d08d7eb
Chengyu Huang <chengyuhuang16@gmail.com>
2017-07-27 14:26:52 -0400

    add charts

--- 0_basic_distribution.py
+++ 0_basic_distribution.py
@@ -14,16 +14,26 @@ tf.reset_default_graph()
 k = tf.placeholder(tf.float32)
 
 ## made a normal distrubituion with a shifting mean 
-mean_moving_normal = tf.random_normal(shape=[100],mean=(10*k),stddev=1)
+mean_moving_normal = tf.random_normal(shape=[100],mean=(10*k),stddev=10)
+## made a normal distrubituion with a expending variance  
+expending_var_normal = tf.random_normal(shape=[100],mean=(10),stddev=10*k)
+## we can also combine two distributions 
+combined_dist = tf.concat([mean_moving_normal,expending_var_normal],axis=0)
+
 ## Record that distribution into a histogram summary 
 tf.summary.histogram("normal/moving_mean",mean_moving_normal)
-summaries = tf.summary.merge_all() 
-#%%
+tf.summary.histogram("normal/expending_var",expending_var_normal)
+tf.summary.histogram("normal/combine",combined_dist)
+
+summaries = tf.summary.merge_all()
 
 ## 
 sess = tf.Session()
 writer = tf.summary.FileWriter("histogram_example")
-N = 20
-for step in range(N):
-    summ = sess.run(summaries,feed_dict={k:step})
+N = 0 
+for step in range(1990,2020):
+    N+=1
+    summ = sess.run(summaries,feed_dict={k:N})
     writer.add_summary(summ,global_step=step)
+
+#%%
